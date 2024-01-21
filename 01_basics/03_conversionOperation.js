3717d69da4950d3d30f205fc1c1362c0d59c14fd
Ranjan yadav <119673937+RanjanYadav5405@users.noreply.github.com>
2024-01-21 18:32:44 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score="Ranjan"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+//"33" => 33
+// "33abc" =>NaN(Not an Number)
+// true =>1; false => 0
+
+let isLoggedIn=1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0=>false;
+// "" => false
+// "Ranjan" =>true
+
+let someNumber =33
+let stringNumber=String(someNumber);
+console.log(stringNumber); // It will show 33 but it is string
+console.log(typeof stringNumber);
